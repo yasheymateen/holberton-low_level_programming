09d2a5749044490c1bdca313468b41d0da312b8d
yasheymateen <yashar.mateen@holbertonschool.com>
2019-06-26 11:43:36 -0700

    modified 0 holberton.c file

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,19 @@
 #include <holberton.h>
+/*
+ * This file prints out the string "Holberton" followed by a new line
+ */
+
 int main(void)
 {
-  _putchar("Holberton");
+  _putchar("H");
+  _putchar("o");
+  _putchar("l");
+  _putchar("b");
+  _putchar("e");
+  _putchar("r");
+  _putchar("t");
+  _putchar("o");
+  _putchar("n");
   _putchar("\n")
   return (0);
 }
