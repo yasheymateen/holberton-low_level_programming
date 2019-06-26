aa5ab4696ae346a288a729071eb5553dc4aa9df7
yasheymateen <yashar.mateen@holbertonschool.com>
2019-06-26 08:49:03 -0700

    modified 4 is alpha file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,7 @@
-#include <holberton.h>
+#include "holberton.h"
+/* This file returns 1 whether the passed argument is a lowercase or uppercase alphabatical character and returns 0 if it is neither of those
+ */
+
 int _isalpha(int c)
 {
   if ((c>='a' && c<= 'z') || (c>='A' && c<='Z'))
