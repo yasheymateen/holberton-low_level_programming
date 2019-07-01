7788ab34cff2b66764d784b1df54626588c5b819
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-01 14:48:28 -0700

    added swap file that swaps value of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * swap - swaps the values of two integers
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+  int temp;
+  temp = *a;
+  *a = *b;
+  *b = temp;
+}
