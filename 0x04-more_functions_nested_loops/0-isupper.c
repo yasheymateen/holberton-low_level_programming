ce975b964b1d847b62c4773df54a9304eb6a67f9
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 14:01:48 -0700

    added readme files, header file and main and 0 isupper file for project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * isupper - this function checks for uppercase character
+ * @c: character to check
+ *
+ * Return: 1 if uppercase, 0 if not
+ */
+int _isupper(int c)
+{
+  return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This folder contains the project assignments for nested loops and using them
