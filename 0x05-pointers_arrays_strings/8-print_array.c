2c90ef2f51d1314b09b80c7fff5eb242af8f2d9f
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 12:04:20 -0700

    added 8 main file and print array file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: int array pointer
+ * @n: int type integer
+ */
+
+void print_array(int *a, int n)
+{
+  int i;
+
+  for (i=0; i<n; i++)
+    {
+      printf("%d", a[i]);
+      if (i<(n-1))
+	{
+	  printf(", ");
+	}
+    }
+  printf("\n");
+}
