7cbdb7d18fb2e8e7dcb2913191964665305b65ae
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 14:32:44 -0700

    added 8 main file and print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+
+#include "holberton.h"
+
+/**
+ * print_square - returns square in the terminal
+ * @size: size of square
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int linenum;
+	int linelen;
+
+	for (linenum = 0; linenum < size; ++linenum)
+	{
+		for (linelen = 0; linelen < size; ++linelen)
+			_putchar('#');
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+		_putchar('\n');
+}
