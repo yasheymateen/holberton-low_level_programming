8c2161a864d92eea23da3887697cfedcea073c1a
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 14:14:45 -0700

    added 2 maiin and multiplication files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - muitlply two numbers.
+ * @a: first multiplication operand.
+ * @b: 2nd multiplication operand
+ *
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
