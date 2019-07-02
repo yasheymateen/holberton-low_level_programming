774018923c9bda56c8db2bc454cf1a2e545667f7
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 15:11:16 -0700

    modified 0 reset to 98 file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,10 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter and updates value to 98
  *
+ * @n: int parameter 
  * return:  void
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+*n = 98;
 }
