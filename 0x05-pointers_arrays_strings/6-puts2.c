0070860cf3ed0638fed59bc66fd92c95e4e59d6c
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 18:17:38 -0700

    modified 6-puts2 file for betty

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
- * puts2.c prints one character out of 2 of a string
- *
+ * puts2 - prints one character out of 2 of a string
+ * @str: string for character to print out
  * Return: void.
  */
 void puts2(char *str)
 {
-  int i=0;
+int i = 0;
 
-  while(*str)
-    {
-      if (i++%2 == 0)
-	_putchar(*str);
-      str++;
-    }
-  _putchar('\n');
+while (*str)
+{
+if (i++ % 2 == 0)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }
