142288f0732baf15795780b301498ff8e863e97b
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 17:51:51 -0700

    modified 5-rev string file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
 /**
 *rev_string- reverses a string.
-*
+* @s: address of string to be reversed
 * Return: void.
 */
 void rev_string(char *s)
 {
-	int i = 0;
-	int j = 0;
-	while (s[j] != '\0')
-	{
-		++i;
-		++j;
-	}
-	--i;
+int i = 0;
+int j = 0;
+while (s[j] != '\0')
+{
+++i;
+++j;
 }
-
+--i;
+}
