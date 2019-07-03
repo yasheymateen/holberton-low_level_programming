c651e0f7f30a923673c320d57f0c3a749b398b96
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 18:26:56 -0700

    modified 100-atoi file for betty

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,38 +2,38 @@
 #include <stdio.h>
 
 /**
- * atoi - converts string into integer
+ * _atoi - converts string into integer
  * @s: string to convert to integer
  *
  * Return: int
  */
 int _atoi(char *s)
 {
-  int n, i, sign;
+int n, i, sign;
 
-  sign = 1;
-  i = 0;
-  n = 0;
+sign = 1;
+i = 0;
+n = 0;
 
-  while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
-    {
-      if (s[i] == '-')
-	sign *= -1;
-      i++;
-    }
-  while ((s[i] >= '0' && s[i] <= '9') && s[i] != '0')
-    {
-      if (n>=0)
-	{
-	  n = n * 10 - (s[i] - '0');
-	  i++;
-	}
-      else
-	{
-	  n = n * 10 - (s[i] - '0');
-	  i++;
-	}
-    }
-  sign*=-1;
-  return (n*sign);
+while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+{
+if (s[i] == '-')
+sign *= -1;
+i++;
+}
+while ((s[i] >= '0' && s[i] <= '9') && s[i] != '0')
+{
+if (n >= 0)
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+else
+{
+n = n * 10 - (s[i] - '0');
+i++;
+}
+}
+sign *= -1;
+return (n *sign);
 }
