47f28d4a06d7f0795cfc8362b4f15bbc8d1a9606
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 17:39:36 -0700

    modified 1 swap file

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,6 +3,9 @@
 /**
  * swap - swaps the values of two integers
  *
+ *Description: swaps values of two integers
+ *@a: address of a value
+ *@b: address of b value
  * Return: void
  */
 void swap_int(int *a, int *b)
