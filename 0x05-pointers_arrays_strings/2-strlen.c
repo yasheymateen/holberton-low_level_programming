8838e32f9c30a3fd92ca0e8b9f8f0df27ea21c43
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 17:43:52 -0700

    modified 2 strlen file to comply with betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
 /**
  * strlen - returns length of a string given a character input
- *
+ * Description: returns length of string given input
+ * @s: address of string input
+ * @length: length of string
  * return - void.
  */
 int _strlen(char *s)
 {
-  int length = 0;
+int length = 0;
 
-  while (*(s + length) != '\0')
-    length++;
+while (*(s + length) != '\0')
+length++;
 
-  return (length);
+return (length);
 }
