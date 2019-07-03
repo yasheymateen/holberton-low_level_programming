b152e7505cc3a3c343b9dab9dbc0807a143983e8
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-02 17:47:40 -0700

    modified 3-puts file

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 
 /**
- * puts - prings a string followed by a new line.
- *
+ * _puts - prings a string followed by a new line.
+ * @str: address of string to be printed
  * Return: void
  */
 void _puts(char *str)
 {
-  int length = 0;
+int length = 0;
 
-  while (*(str + length) !='\0')
-    {
-      _putchar(*(str + length));
-      length++;
-    }
-  _putchar('\n');
+while (*(str + length) != '\0')
+{
+_putchar(*(str + length));
+length++;
+}
+_putchar('\n');
 }
