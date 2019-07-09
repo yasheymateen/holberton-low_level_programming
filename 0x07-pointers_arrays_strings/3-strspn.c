74af1003945e39667b29b20d1742fcbae2f1e217
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 13:56:30 -0700

    modified 3-strspn file to comply with betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,12 +11,12 @@ unsigned int _strspn(char *s, char *accept)
 unsigned int a, b;
 
 a = 0;
-while(s[a] != '\0')
+while (s[a] != '\0')
 {
 b = 0;
 while (accept[b] != '\0' && s[a] != accept[b])
 b++;
-if (accept [b] == '\0')
+if (accept[b] == '\0')
 return (a);
 a++;
 }
