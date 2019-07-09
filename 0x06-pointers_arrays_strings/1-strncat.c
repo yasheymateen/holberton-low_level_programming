1f3fb13cbdfee9bfe3b86ce821a0868816c8c180
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 14:57:54 -0700

    added 1-main file and strncat file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+/**
+ * _strncpy - concatenates two strings.
+ * @dest: destination string.
+ * @src: source string.
+ * @n: number of bytes to use.
+ * Return: pointer to resulting string.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int a, b;
+
+a = 0;
+for (b = 0; dest[b] != '\0'; b++)
+;
+while (src[a] != '\0' && a < n)
+{
+dest[a + b] = src[a];
+a++;
+}
+return (dest);
+}
