aa3b30a28cfd273f3ddef4bdadb0db8371ae12f9
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 19:06:32 -0700

    adding 9 main file and set string file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - function that sets the value of pointer to a char
+ * @s: pointer s
+ * @to: pointer to
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
