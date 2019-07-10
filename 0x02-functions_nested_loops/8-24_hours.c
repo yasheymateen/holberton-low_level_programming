a17dc5c6936195952140d640128140950c25935f
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 11:54:23 -0700

    added 8-24 hours file

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,23 @@
-#include <holberton.h>
+#include "holberton.h"
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer
+ * Return: void
+ */
 
 void jack_bauer(void)
 {
-  int i, x;
-  for (i=0; i<24; i++)
-    {
-      for (x=0; x<60; x++)
-	{
-	  _putchar((i/10)+'0');
-	  _putchar((i%10)+'0');
-	  _putchar(':');
-	  _putchar((x/10)+'0');
-	  _putchar((x%10)+'0');
-	  _putchar('\n');
-	}
-    }
+int i, x;
+
+for (i = 0; i < 24; i++)
+{
+for (x = 0; x < 60; x++)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((x / 10) + '0');
+_putchar((x % 10) + '0');
+_putchar('\n');
+}
+}
 }
