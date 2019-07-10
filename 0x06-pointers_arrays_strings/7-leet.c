43a2f98b1af664bb8bf92a9d99e3b107f7d2fd35
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 22:50:18 -0700

    modified header file and added 7-main and 7-leet file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * leet - function that encodes a string into 1337
+ * @a: string to encode
+ * Return: character string
+ */
+
+char *leet(char *a)
+{
+  int i, x;
+  char enc[] = "aAeEoOtTlL";
+  char num[] = "43071";
+
+  i = 0;
+  for (i = 0; a[i] != '\0'; i++)
+    {
+      for (x = 0; enc[x] != '\0'; x++)
+	if (a[i] == enc[x])
+	  a[i] = num[x / 2];
+    }
+  return (a);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -12,5 +12,7 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
 char *cap_string(char *a);
+char *leet(char *a);
+char *rot13(char *a);
 
 #endif /* HOLBERTON_H */
