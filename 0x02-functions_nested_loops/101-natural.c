20b758eb5204fc4e47790e3956f5bf637645f273
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 12:15:51 -0700

    added 101 natural file

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * main - lists all natural numbers multiples of 3 or 5
+ * Return: void
+ */
+
 int main(void)
 {
-  int i, x;
-  for (i=0; i<1024; i++)
-    {
-      if (i%3==0 || i%5==0)
-	x+=i;
-    }
-  printf("%d\n", x);
-  return(0);
+int i, x;
+for (i = 0; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+x += i;
+}
+printf("%d\n", x);
+return (0);
 }
