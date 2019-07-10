4ed9e6fbc9b1bff2bc444ec93e51747076027f18
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 12:12:20 -0700

    modified 11 file and added main

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: natural numbers
+ * Return: void
+ */
 
 void print_to_98(int n)
 {
-  if(n<98)
-    {
-      for (n=n; n<98; n++)
-	printf("%d, ", n);
-      printf("%d\n", 98);
-    }
-  else if(n >98)
-    {
-      for(n=n; n>98; n--)
-	{
-	  printf("%d, ", n);
-	}
-      printf("%d\n", 98);
-    }
-  else
-    {
-      printf("%d\n", 98);
-    }
+if (n < 98)
+{
+for (n = n; n < 98; n++)
+printf("%d, ", n);
+printf("%d\n", 98);
+}
+else if (n > 98)
+{
+for (n = n; n > 98; n--)
+{
+printf("%d, ", n);
+}
+printf("%d\n", 98);
+}
+else
+{
+printf("%d\n", 98);
+}
 }
