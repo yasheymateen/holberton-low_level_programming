862d8cd6166150a3fa5eda8f27dd63f70dcdc2a2
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 23:15:40 -0700

    added print number file, and main file

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * print_number - function that prints an integer
+ * @n: integer to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+  int neg, mul;
+
+  mul = 1000000000;
+  neg = 1;
+  if (n > 0)
+    {
+      n *= -1;
+    }
+  if (n != 0)
+    {
+      while ((n / mul) == 0)
+	{
+	  mul /= 10;
+	}
+      if (neg == 1)
+	_putchar('-');
+      while (mul >= 1)
+	{
+	  _putchar (-(n / mul) + '0');
+	  n %= mul;
+	  mul /= 10;
+	}
+    }
+  else
+    _putchar('0');
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,5 +14,8 @@ char *string_toupper(char *a);
 char *cap_string(char *a);
 char *leet(char *a);
 char *rot13(char *a);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* HOLBERTON_H */
