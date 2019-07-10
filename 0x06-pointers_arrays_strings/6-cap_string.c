866808c09429d0f367755dd4b364172b581a23ce
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 22:42:52 -0700

    modified 6-capstring to comply with betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,18 +7,18 @@
 
 char *cap_string(char *a)
 {
-  int i, x;
-  char ind[] = " \t\n,;.!/\"(){}";
+int i, x;
+char ind[] = " \t\n,;.!/\"(){}";
 
-  i = 1;
-  if (a[0] >= 'a' && a[0] <= 'z')
-    a[0] -= ('a' - 'A');
-  while (a[i] != '\0')
-    {
-      for (x = 0; ind[x] != '\0'; x++)
-	if(a[i - 1] == ind[x] && (a[i] >= 'a' && a[i] <= 'z'))
-	  a[i] -= ('a' - 'A');
-      i++;
-    }
-  return (a);
+i = 1;
+if (a[0] >= 'a' && a[0] <= 'z')
+a[0] -= ('a' - 'A');
+while (a[i] != '\0')
+{
+for (x = 0; ind[x] != '\0'; x++)
+if(a[i - 1] == ind[x] && (a[i] >= 'a' && a[i] <= 'z'))
+a[i] -= ('a' - 'A');
+i++;
+}
+return (a);
 }
