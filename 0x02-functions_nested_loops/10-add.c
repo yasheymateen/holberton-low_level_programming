3e5cd9bbb9c00fc889dabc286e0de34b5eed0684
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 11:57:54 -0700

    modified header file and added 10 add file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,12 @@
-#include <holberton.h>
+#include "holberton.h"
+/**
+ * add - function that adds two integers
+ * @a: integer a
+ * @b: integer b
+ * Return: result
+ */
+
 int add(int a, int b);
 {
-  return (a+b);
+return (a + b);
 }
