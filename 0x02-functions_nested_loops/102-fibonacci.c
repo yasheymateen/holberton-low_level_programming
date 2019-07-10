ee90bb5e41b86abdcc11b2950005c26409807c15
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 12:19:37 -0700

    added 102 fibonacci file

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,19 +1,25 @@
-#include <stdio.h)
+#include <stdio.h>
+/**
+ * main - print the first 50 fibonacci numbers
+ * Return: void
+ */
+
 int main(void)
 {
-  unsigned long counter, a,b,c;
-  a=0;
-  b=1;
-  for(counter=0; counter<50; counter++)
-    {
-      c=a+b;
-      a=b;
-      b=c;
-      printf("%lu", c);
-      if (counter==49)
-	putchar('\n');
-      else
-	printf(", ");
-    }
-  return (0);
+unsigned long counter, a, b, c;
+a = 0;
+b = 1;
+
+for (counter = 0; counter < 50; counter++)
+{
+c = a + b;
+a = b;
+b = c;
+printf("%lu", c);
+if (counter == 49)
+putchar('\n');
+else
+printf(", ");
+}
+return (0);
 }
