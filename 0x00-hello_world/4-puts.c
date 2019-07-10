b58a2c8224ed454c60d7a93505da59ad093780ba
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 10:39:59 -0700

    modified 4-puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * puts - Prints out string 
+ * puts - Prints out string
  * Return: void
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
