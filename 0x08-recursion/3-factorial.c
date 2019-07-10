e5be027fc6852f3edecc94446a3aa428db727fae
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 15:04:47 -0700

    modifying 3 main file and factorial file

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: integer to get factorial
+ * Return: -1 if error
+ */
+
+int factorial(int n)
+{
+if (n > 0)
+return (n *= factorial(n - 1));
+else if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+return (n);
+}
