65165a4857552ddf0193f761efbabf800105b3eb
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 18:59:53 -0700

    modified 8 print diagonal file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,9 +12,9 @@ void print_diagsums(int *a, int size)
 
   sum1 = 0;
   sum2 = 0;
-  for (i = 0; i < (size^2); i += size + 1)
+  for (i = 0; i < (size * size); i += size + 1)
     sum1 += a[i];
-  for (i = size - 1; i < (size ^2 - 1); i += size - 1)
+  for (i = size - 1; i < (size * size - 1); i += size - 1)
     sum2 += a[i];
   printf("%d, %d\n", sum1, sum2);
 }
