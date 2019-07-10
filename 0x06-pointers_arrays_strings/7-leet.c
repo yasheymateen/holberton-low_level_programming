e131a24b2f89551418748965cc0d54a7c9ea4518
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-09 22:51:33 -0700

    modified 7-leet for betty

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,16 @@
 
 char *leet(char *a)
 {
-  int i, x;
-  char enc[] = "aAeEoOtTlL";
-  char num[] = "43071";
+int i, x;
+char enc[] = "aAeEoOtTlL";
+char num[] = "43071";
 
-  i = 0;
-  for (i = 0; a[i] != '\0'; i++)
-    {
-      for (x = 0; enc[x] != '\0'; x++)
-	if (a[i] == enc[x])
-	  a[i] = num[x / 2];
-    }
-  return (a);
+i = 0;
+for (i = 0; a[i] != '\0'; i++)
+{
+for (x = 0; enc[x] != '\0'; x++)
+if (a[i] == enc[x])
+a[i] = num[x / 2];
+}
+return (a);
 }
