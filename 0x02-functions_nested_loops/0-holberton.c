f236013041f4fc411deee6d7a254bf0b291bddc9
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-10 11:19:10 -0700

    re-modified files

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,17 @@
 #include "holberton.h"
-
 /**
- * main - entry point
- *
+ * main - prints Holberton followed by new line
  * Return: 0
  */
 int main(void)
 {
-	char *holberton;
+char c[] = "Holberton";
+int i;
 
-	for (holberton = "Holberton\n"; *holberton != '\0'; ++holberton)
-		_putchar(*holberton);
-	return (0);
+for (i = 0; i < 9; i++)
+{
+_putchar(c[i]);
+}
+_putchar('\n');
+return (0);
 }
