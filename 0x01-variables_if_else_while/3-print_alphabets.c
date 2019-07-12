f077c296ae8cb3957782a77bdc0f2cbbdd91ea7d
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-11 19:52:08 -0700

    added 3-print alphabets file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function that prints alphabet in lowercase and then in uppercase
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <=122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
