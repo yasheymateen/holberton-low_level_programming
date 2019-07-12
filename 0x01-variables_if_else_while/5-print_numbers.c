c2c6631543f29910330b096692af9637157dec04
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-11 20:02:20 -0700

    added 5 print numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function that prints all single digit integers
+ * Return: void
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
