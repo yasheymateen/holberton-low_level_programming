e09a30a9d4dd8a3de4015c4da7d6adfbf5587f80
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-11 19:44:52 -0700

    added 2-print alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - function that prints the alphabet in lowercase
+ * Return: void
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
