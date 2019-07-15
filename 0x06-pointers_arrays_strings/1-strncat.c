ba112f49f7378e719c90029fb091399fa5e09156
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-15 00:58:19 -0700

    modified 1 strncat file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strncpy - concatenates two strings.
+ * _strncat - concatenates two strings.
  * @dest: destination string.
  * @src: source string.
  * @n: number of bytes to use.
