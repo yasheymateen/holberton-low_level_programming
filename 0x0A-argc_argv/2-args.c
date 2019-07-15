eab2ddff6bc5407fcb16bdb54c1b647c5c5e356b
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-14 21:21:51 -0700

    added task 2 argsc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @arc: # of arguments
+ * @argv: array of arguments
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
