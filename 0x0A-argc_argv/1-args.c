21f8ad58b169ac73e2afbfbf6188b556ab29056c
yasheymateen <yashar.mateen@holbertonschool.com>
2019-07-14 21:09:25 -0700

    added task 1 argc file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: # of arguments
+ * @argv: array containing arguments
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
