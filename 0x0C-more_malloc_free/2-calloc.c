d27836eaaa48e3e6ebac275e477d67880ae03d41
Yashar Mateen <yasheymateen>
2019-07-19 04:33:06 +0000

    adding 2 main file and 2 calloc file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _calloc - function that allocates memory for an array using malloc
+ * @nmemb: # of elements to allocate memory for
+ * @size: size of array
+ * Return: array that is mallocated
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	int *n;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size)
+		if (arr == NULL)
+			return (NULL);
+	n = arr;
+	while (*n != '\0')
+	{
+		*n = 0;
+		n++;
+	}
+	return (arr);
+}
