c7bda03a34e844e78b749365e8f4864fedecba3f
Yashar Mateen <yasheymateen>
2019-07-19 05:21:01 +0000

    added 100 main file and 100 realloc file

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _realloc - function that reallocates memory block
+ * @ptr: pointer to old block of memory
+ * @old_size: size of old memory block
+ * @new_size: size of new block of memory
+ * Return: pointer to new block of memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+{
+
+	unsigned int i;
+	char *arr, *nptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (!new_size && ptr)
+	{
+		free (ptr);
+		return (NULL);
+	}
+	nptr = ptr;
+	if (arr == NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (arr);
+	for (i = 0; i < old_size && i < new_size; i++)
+		arr[i] = nptr[i];
+	arr = (void *) arr;
+	free (ptr);
+	return (arr);
+}
