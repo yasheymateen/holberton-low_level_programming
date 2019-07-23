ffdce15d1bc61ef903660b8128b0f8ef5fd874f7
Yashar Mateen <yasheymateen>
2019-07-23 05:59:18 +0000

    added 1 main file and 1 init dog file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * initialize struct variable
+ * @d: structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
