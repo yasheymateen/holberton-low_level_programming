83a1121f6e725d94be8c4c70bcb15f0873ada6e1
Yashar Mateen <yasheymateen>
2019-07-25 17:50:28 +0000

    modified 3 get op func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -6,7 +6,14 @@
  * @s: pointer
  * Return: pointer to function to use
  */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
 
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0' && s1[a] == s2[a]; a++)
+		;
+	return (s1[a] - s2[a]);
+}
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
@@ -22,7 +29,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (*s == ops[i].op[0])
+		if (_strcmp(ops[i].op, s)
 			return (ops[i].f);
 		i++;
 	}
