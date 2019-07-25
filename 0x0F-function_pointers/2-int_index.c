b5c6ad6c6db055af42cfc1339acb12cf0d8b5cd8
Yashar Mateen <yasheymateen>
2019-07-25 04:24:31 +0000

    added 2 main file and 2 int index file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - function that searches for an integer
+ * @array: array of ints
+ * @size: size of array
+ * @cmp: function that compares values
+ * Return: -1 if fails, otherwise index of value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || !size || !cmp)
+		return;
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
