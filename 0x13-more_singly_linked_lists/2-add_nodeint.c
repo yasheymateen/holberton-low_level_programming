38a036a578910a1cefc426737f04df530011b430
Yashar Mateen <yasheymateen>
2019-08-07 03:39:15 +0000

    added 2 add node file

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_nodeint - add node to beginning of list
+ * @head: linked list
+ * @n: new value
+ * Return: new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
