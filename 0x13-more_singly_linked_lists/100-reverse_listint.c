dfa9c19cecdc8a6f658bb3cabd17bd2c2e4f32f8
Yashar Mateen <yasheymateen>
2019-08-07 04:07:37 +0000

    added modified lists file and reverse list int file

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *reverse_listint - reverses a linked list
+ *@head: head of the list
+ *Return: list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *stemp;
+
+	temp = *head;
+	if (temp == NULL)
+		return (NULL);
+	while (temp->next != NULL)
+	{
+		stemp = temp->next;
+		temp->next = stemp->next;
+		stemp->next = *head;
+		*head = stemp;
+	}
+	return (*head);
+}
