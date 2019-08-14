9623f2f8868547a96f1af397b1941441ed99b7e7
Yashar Mateen <yasheymateen>
2019-08-14 05:21:29 +0000

    added 3 file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,41 +1,44 @@
 #include "holberton.h"
-#include <stdio.h>
-/**
- * main - function that copies content of file to another
- * @ac: argument count
- * @av: array of argument tokens
- * Return: 0 on success
- */
-int main(int ac, char *av[])
-{
-	int fd_from, fd_to, rd_stat, wr_stat;
-	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
-	char buffer[BUFSIZE];
 
-	if (ac != 3)
+/**
+ * main - function that copies info from one file to another
+ * @argc: number of arguments
+ * @ar: value of the arguments
+ * Return: 0 for success
+ */
+int main(int argc, char **ar)
+{
+	int fp_from, fp_to, j;
+	ssize_t write_from, write_to;
+	char buffer[BUFFER_SIZE];
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
-	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	rd_stat = 1;
-	while (rd_stat)
+	fp_from = open(ar[1], O_RDONLY);
+	if (fp_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ar[1]), exit(98);
+	fp_to = open(ar[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fp_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ar[2]), exit(99);
+	write_from = 1;
+	while (write_from)
 	{
-		rd_stat = read(fd_from, buffer, BUFSIZE);
-		if (rd_stat == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
-		if (rd_stat > 0)
+		write_from = read(fp_from, buffer, BUFFER_SIZE);
+		if (write_from == -1)
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", ar[1]), exit(98);
+		if (write_from > 0)
 		{
-			wr_stat = write(fd_to, buffer, rd_stat);
-			if (wr_stat != rd_stat || wr_stat == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			write_to = write(fp_to, buffer, write_from);
+			if (write_to == -1 || write_from != write_to)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", ar[2]), exit(99);
 		}
-	}
-	if (close(fd_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
-	if (close(fd_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
-	return (0);
-}
+		}
+			j = close(fp_from);
+			if (j == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_from), exit(100);
+			j = close(fp_to);
+			if (j == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fp_to), exit(100);
+			return (0);
+		}
