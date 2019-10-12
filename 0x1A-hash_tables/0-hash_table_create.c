bbee8c253f0045ad7185a5dc9777369d5433ef02
Yashar Mateen <yasheymateen>
2019-10-12 00:46:02 +0000

    added readme file and 1st task

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of array
+ * Return: pointer to new hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int i;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return NULL;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	hash_table->size = size;
+	return (hash_table);
+}
