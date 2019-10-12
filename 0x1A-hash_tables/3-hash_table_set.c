de121032e8284bf63fc9d11094dce72906b75d64
Yashar Mateen <yasheymateen>
2019-10-12 02:10:18 +0000

    modified task 3 for betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-       	unsigned long int index;
+	unsigned long int index;
 	hash_node_t *temp, *new;
 
 	if (key == NULL || strlen(key) == 0 || value == NULL || ht == NULL)
