2f10788c6c42f9fc8ab305b6b69878c27e4fa317
Yashar Mateen <yasheymateen>
2019-11-15 23:43:05 +0000

    modified 5 island perimeter for pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,10 +9,10 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                perimeter +=4
+                perimeter += 4
                 if x-1 >= 0 and grid[x-1][y] == 1:
                     perimeter -= 1
-                if x+1 <len(grid) and grid[x+1][y] == 1:
+                if x+1 < len(grid) and grid[x+1][y] == 1:
                     perimeter -= 1
                 if y-1 >= 0 and grid[x][y-1] == 1:
                     perimeter -= 1
